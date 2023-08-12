52a11d6366d011d59c87cf6533669d4bee8561d2
Prerna Pranav <prernapranav.bgp@gmail.com>
2023-08-12 09:10:23 +0530

    episode2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag."),
-    React.createElement("h2", {}, "I'm an h2 tag."),
+    React.createElement("h1", {}, "This is  Namaste React 🚀."),
+    React.createElement("h2", {}, "by Prerna Pranav."),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag."),
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# namaste-react
+# Namaste React 🚀
+
 Repository for my React learning journey - assignment nodes and code from the Namaste React course
+
+# Parcel
+
+-dev build
+-local server
+-HMR = Hot Module Replacement
+-file watching algorithm(written in c++)
+-caching - faster builds
+-image optimization
+-minification
+-bundling
+-compress
+-consistent hashing
+-code splitting
+-differential bundling - support older browsers
+-diagnostic
+-error handling
+-HTTPs
+-tree shaking
